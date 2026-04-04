4c6839c3b29793eb12284c5d62b0e868e2337c3a
saraahmed0706-2 <saraahmed0706apseme@gmail.com>
2026-04-05 02:31:38 +0500

    fix: resolve Swagger reference error in Program.cs

--- mywebapp/src/Program.cs
+++ mywebapp/src/Program.cs
@@ -1,11 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -14,9 +11,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
