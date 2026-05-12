feb78e1c017434ca2c9103b3cb91b8f61c317654
saraahmed0706-2 <saraahmed0706apseme@gmail.com>
2026-05-12 09:51:38 +0500

    Add Swagger and update service configuration
    Updated the Program.cs to add Swagger support and modify service registrations.

--- mywebapp/src/Program.cs
+++ mywebapp/src/Program.cs
@@ -1,26 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
